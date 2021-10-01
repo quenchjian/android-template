996227404fed80b4c25115f8429dd60d51d7c984
quenchjian <quench.jian@gmail.com>
2021-10-01 12:11:00 +0800

    Refactor: fine tune background thread executor

--- app/src/main/java/me/quenchjian/concurrent/DefaultScheduler.kt
+++ app/src/main/java/me/quenchjian/concurrent/DefaultScheduler.kt
@@ -4,6 +4,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.asCoroutineDispatcher
 import java.util.concurrent.Executors
 import java.util.concurrent.ThreadPoolExecutor
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -14,6 +15,8 @@ class DefaultScheduler @Inject constructor() : Schedulers {
 
   init {
     executor.corePoolSize = 3
+    executor.maximumPoolSize = Int.MAX_VALUE
+    executor.setKeepAliveTime(60, TimeUnit.SECONDS)
   }
 
   override val io = executor.asCoroutineDispatcher()
