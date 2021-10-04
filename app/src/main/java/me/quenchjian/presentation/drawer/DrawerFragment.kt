b549e454fb319a99deb69a22814c7ee24e562456
quenchjian <quench.jian@gmail.com>
2021-10-04 15:16:45 +0800

    Fix Bug: fix compile errors

--- app/src/main/java/me/quenchjian/presentation/drawer/DrawerFragment.kt
+++ app/src/main/java/me/quenchjian/presentation/drawer/DrawerFragment.kt
@@ -2,21 +2,16 @@ package me.quenchjian.presentation.drawer
 
 import android.os.Bundle
 import androidx.annotation.LayoutRes
-import dagger.hilt.android.AndroidEntryPoint
-import dagger.hilt.android.EntryPointAccessors
 import me.quenchjian.navigation.FragmentKey
 import me.quenchjian.navigation.KeyedFragment
-import me.quenchjian.navigation.Navigator
+import me.quenchjian.navigation.navigator
 import me.quenchjian.presentation.statistics.StatisticsFragment
 import me.quenchjian.presentation.tasks.TasksFragment
-import java.util.EnumMap
-import javax.inject.Inject
+import java.util.*
 
 abstract class DrawerFragment<V : DrawerScreen>(@LayoutRes id: Int) : KeyedFragment(id) {
 
-  protected val navProvider: Navigator.Provider by lazy { Navigator.Provider.get(requireContext()) }
-
-  abstract val view: V
+  abstract val drawer: V
   private val targets = EnumMap<DrawerScreen.Menu, FragmentKey>(DrawerScreen.Menu::class.java)
 
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,11 +22,10 @@ abstract class DrawerFragment<V : DrawerScreen>(@LayoutRes id: Int) : KeyedFragm
 
   override fun onStart() {
     super.onStart()
-    view.onActionClick { action ->
+    drawer.onActionClick { action ->
       if (action == getCurrentMenu()) {
         return@onActionClick
       }
-      val navigator = navProvider.get(this)
       when (action) {
         DrawerScreen.Menu.TASKS -> navigator.goBack()
         DrawerScreen.Menu.STATISTICS -> navigator.goTo(targets[action]!!)
